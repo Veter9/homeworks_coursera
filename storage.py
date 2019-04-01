47950d85d700470d12ef367bc35985064862796a
Alexander kryuchkov <kruchkov.alexandr@gmail.com>
2019-04-01 22:26:10 +0300

    add nargs

--- storage.py
+++ storage.py
@@ -6,33 +6,20 @@ import tempfile
 #waiting for new variables to input 
 parser = argparse.ArgumentParser()
 parser.add_argument('--key', action='store', dest='key', help='key')
-parser.add_argument('--val', action='store', dest='val', help='value')
+parser.add_argument('--val', nargs='+', action='store', dest='val', help='value')
 results = parser.parse_args()
-#vars
 
-data = {}  
-data['store1'] = []  
-data['store1'].append({  
-    results.key: results.val
-})
+input_first = json.dumps({results.key: results.val}, sort_keys=True, indent=4)
 
-with open('data.txt', mode='a+') as json_file:
-    json_file = {results.key: results.val}
-    json.dump(data, json_file)
-  
-with open('data.txt') as json_file:  
-    data = json.load(json_file)
-    for p in data['store1']:
-        print(results.key + p[results.key])
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 
 
-# if results.val == None:
-#     with open(storage_path, 'r+') as f:
-#         # print(f.read())
-#         print("1")
-# else:
-#     with open(storage_path, 'r+') as f:
-#         # f.write(results.key)
-#         # f.seek(0)
-#         print("2")
+if results.val == None:
+    with open(storage_path, 'r+') as f:
+        f.seek(0)
+        print(f.read())
+
+else:
+    with open(storage_path, 'r+') as f:
+        f.write(input_first)
 
