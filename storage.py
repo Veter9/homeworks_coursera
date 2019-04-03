a2178dde2e7c3372e1342f02d705520a84cbb799
Alexander kryuchkov <kruchkov.alexandr@gmail.com>
2019-04-03 20:08:36 +0300

    fixed

--- storage.py
+++ storage.py
@@ -8,26 +8,29 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--key', action='store', dest='key', help='key')
 parser.add_argument('--val', nargs='+', action='store', dest='val', help='value')
 results = parser.parse_args()
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+
+
+def write_json(data, filename=storage_path):
+    with open(filename,'a+') as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
 
 new_data = {results.key: [results.val]}
-print(new_data)
-print("1111111111111111111")
-json_view = json.dumps(new_data, sort_keys=True, indent=4)
-print(json_view)
-print("2222222222222222222")
-storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+
 
 with open(storage_path) as json_file:
     old_data = json.load(json_file)
-    
-print(old_data)
+    new_data.update(old_data)
+    json_file.seek(0)
+os.remove(os.path.join(tempfile.gettempdir(), "storage.data"))   
+write_json(new_data)    
 
-if results.key in old_data:
-    print("exist")
-    print(results.val)
-else:
-    print("not exist")
-    with open(storage_path, 'a+') as d:
-        d.write(new_data)
-        d.close()
 
+# def write_json(data, filename='answer.json'):
+#     with open(filename,'w') as f:
+#     json.dump(data, f, indent=2, ensure_ascii=False)
+
+# with open('/home/***/answer.json') as json_file:
+#     old_data= json.load(json_file)
+#     newdata.update(old_data)
+# write_json(newdata)    
