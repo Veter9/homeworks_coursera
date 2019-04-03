caa0c4e3fff3aef3b4c6a3365928c22a8bf1b3a4
Alexander kryuchkov <kruchkov.alexandr@gmail.com>
2019-04-03 06:37:57 +0300

    new file

--- storage.py
+++ storage.py
@@ -9,17 +9,25 @@ parser.add_argument('--key', action='store', dest='key', help='key')
 parser.add_argument('--val', nargs='+', action='store', dest='val', help='value')
 results = parser.parse_args()
 
-input_first = json.dumps({results.key: results.val}, sort_keys=True, indent=4)
-
+new_data = {results.key: [results.val]}
+print(new_data)
+print("1111111111111111111")
+json_view = json.dumps(new_data, sort_keys=True, indent=4)
+print(json_view)
+print("2222222222222222222")
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 
+with open(storage_path) as json_file:
+    old_data = json.load(json_file)
+    
+print(old_data)
 
-if results.val == None:
-    with open(storage_path, 'r+') as f:
-        f.seek(0)
-        print(f.read())
-
+if results.key in old_data:
+    print("exist")
+    print(results.val)
 else:
-    with open(storage_path, 'r+') as f:
-        f.write(input_first)
+    print("not exist")
+    with open(storage_path, 'a+') as d:
+        d.write(new_data)
+        d.close()
 
